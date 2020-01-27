c89fe05eec9daca0bb73782faa04b3908f8cf5d9
berracuda <32536668+berracuda@users.noreply.github.com>
2020-01-27 16:26:51 +0700

    27012020

---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+console.log('hello')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
